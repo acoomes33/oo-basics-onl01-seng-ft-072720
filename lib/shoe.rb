1ab95d6bc65f815bb67616430a17833eb53a5cc7
Alan Coomes <acoomes1@cord.edu>
2020-08-06 22:15:49 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,6 @@
 # Make your shoe class here!
 class Show
-   attr_accessor :color, :, :condition
+   attr_accessor :color, :size, :material, :condition
    attr_reader :brand
    
   def initialize(brand)
